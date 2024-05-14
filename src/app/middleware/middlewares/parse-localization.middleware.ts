6e2a1b2552d2db34f8f4f9db70cef4ac794c9f56
sergiuchilat <chilatsergiu@gmail.com>
2024-05-14 16:27:44 +0300

    Remove request token role verification

--- src/app/middleware/middlewares/parse-localization.middleware.ts
+++ src/app/middleware/middlewares/parse-localization.middleware.ts
@@ -1,8 +1,6 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 
 import { NextFunction, Request, Response } from 'express';
-import { Language } from '@/app/enum/language.enum';
-import { setLanguage } from '@/app/utils/localization';
 
 @Injectable()
 export class ParseLocalizationMiddleware implements NestMiddleware {
