d1d8d1d05658b24d16359f72d2a0f2a48de0da05
Daniel Maga <daniel.maga@icdsoft.ro>
2025-02-21 14:52:08 +0200

    WHYYYYYYYYYYYYYYYYYYYY 😔😔😔

--- src/database/migrations/1739795192489-RemoveReceiverUuidUniqueForInternalNotificationReceiver.ts
+++ src/database/migrations/1739795192489-RemoveReceiverUuidUniqueForInternalNotificationReceiver.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class RemoveChatIdUniqueForTelegramReceiver1739795192489 implements MigrationInterface {
+export class RemoveReceiverUuidUniqueForInternalNotificationReceiver1739795192489 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `ALTER TABLE internal_notification_receivers DROP CONSTRAINT "UQ_ce5ea0b81669df3d454229f55e8"`,
